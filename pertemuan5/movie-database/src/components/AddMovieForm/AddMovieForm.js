251f1b99f3c5639a4ba2ffb52a774ca47c31bc22
annisam23 <annisamrahma23@gmail.com>
2023-03-23 14:30:22 +0700

    adding AddMovieForm

---
+++ pertemuan5/movie-database/src/components/AddMovieForm/AddMovieForm.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/heading-has-content */
+import styles from "./AddMovieForm.module.css";
+
+const AddMovieForm = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <img className={styles.left__image}
+            src="https://picsum.photos/417/374"/>
+            </div>
+
+            <div className={styles.right}>
+                <h2 className={styles.right__title}>Add Movie</h2>
+            <form action="#">
+                <div className={styles.right_formcontrol}>
+                    <label for="title">Title</label><br></br>
+                    <input id="title" type="text" className={styles.right__inputform}></input>
+                </div>
+                <div className={styles.right_formcontrol}>
+                <label for="year">Year</label><br></br>
+                    <input id="year" type="text" className={styles.right__inputform}></input>
+                </div>
+                <button type="submit" className={styles.form__button}>Submit</button>
+            </form>
+            </div>   
+        
+        </div>
+    )
+}
+export default AddMovieForm;
