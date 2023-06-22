a5a6f8c4b3148696fe8937296da247c4e6c99119
annisam23 <annisamrahma23@gmail.com>
2023-06-22 10:06:16 +0700

    add s

--- pertemuan14/movie-database/src/utils/constants/endpoint.js
+++ pertemuan14/movie-database/src/utils/constants/endpoint.js
@@ -1,5 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = "http://api.themoviedb.org/3";
+const BASE_URL = "https://api.themoviedb.org/3";
 
 const ENDPOINTS = {
     POPULAR: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
